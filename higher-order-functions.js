1ae4918e1768eda052487271edc8583c2b26a658
Adrianna Malkiewicz <adrianna.malkiewicz@codilime.com>
2019-07-15 10:51:59 +0200

    Higher-order functions task

---
+++ higher-order-functions.js
@@ -0,0 +1,32 @@
+const animals = [
+  { name: 'Fluffykins', species: 'rabbit',	years: 2 },
+  { name: 'Caro',       species: 'dog',		years: 5 },
+  { name: 'Hamilton',   species: 'dog',		years: 1 },
+  { name: 'Harold',     species: 'fish',	years: 0.5 },
+  { name: 'Ursula',     species: 'cat',		years: 4 },
+  { name: 'Jimmy',      species: 'fish', 	years: 1 }
+];
+
+/*
+ * Task 1.1
+ * Input: array of animals `animals`
+ * Output: array of animal names
+ *
+ * Please use `map` function
+ */
+
+/*
+ * Task 1.2
+ * Input: array of animals `animals`
+ * Output: array of dogs
+ *
+ * Please use `filter` function
+ */
+
+/*
+ * Task 1.3
+ * Input: array of animals `animals`
+ * Output: an object, where a key is species and a value is it's average age
+ *
+ * Please use `reduce` function
+ */
